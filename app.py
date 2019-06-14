72fd7e9f9449f35b516fde9dc3bbaa0e7b686ad7
Tomas <xxxxx@xxxx.com>
2019-06-14 20:48:17 -0300

    bit better design

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     return render_template("index.html")
 
-@app.route("/horror")
-def horror():
-    url = "https://www.imdb.com/search/title?genres=horror&sort=user_rating,desc&title_type=feature&num_votes=25000,"
+@app.route('/<categoria>')
+def categoria(categoria):
+    if categoria == "horror":
+        url = "https://www.imdb.com/search/title?genres=horror&sort=user_rating,desc&title_type=feature&num_votes=25000,"
+    elif categoria == "action":
+        url = "https://www.imdb.com/search/title?title_type=feature&num_votes=25000,&genres=action&sort=user_rating,desc"
+    elif categoria == "scifi":
+        url = "https://www.imdb.com/search/title?genres=sci_fi&sort=user_rating,desc&title_type=feature&num_votes=25000,"
+    
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, features="lxml")
 
@@ -32,54 +37,13 @@ def horror():
     llist = soup.find_all('span',{'class':'lister-item-year'})   
     anio = llist[x].text         
 
+    nombre += anio
 
     print(nombre,"\\\\", rating)
 
-    return render_template('result.html', nombre=nombre, rating=rating, duracion=duracion, anio=anio)
-
-@app.route("/action")
-def action():
-    url = "https://www.imdb.com/search/title?title_type=feature&num_votes=25000,&genres=action&sort=user_rating,desc"
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, features="lxml")
-    
-    x = random.randint(0,49)
-    
-    #nombre
-    llist = soup.find_all('h3',{'class':'lister-item-header'})
-    nombre = llist[x].find('a').text
-    #rating
-    llist = soup.find_all('div',{'class':'inline-block ratings-imdb-rating'})
-    rating = llist[x].find('strong').text
-    
-    print(nombre,"\\\\", rating)
-
-    return render_template('result.html', nombre=nombre, rating=rating)
-
-@app.route("/scifi")
-def scifi():
-    url = "https://www.imdb.com/search/title?genres=sci_fi&sort=user_rating,desc&title_type=feature&num_votes=25000,"
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, features="lxml")
-    
-    x = random.randint(0,49)
-    llist = []
-    tiempo = ""
-    #nombre
-    llist = soup.find_all('p',{'class':'text-muted'})
-    tiempo = llist[x].find('span',{'class':'runtime'}).text
-    #nombre
-    llist = soup.find_all('h3',{'class':'lister-item-header'})
-    nombre = llist[x].find('a').text
-    #rating
-    llist = soup.find_all('div',{'class':'inline-block ratings-imdb-rating'})
-    rating = llist[x].find('strong').text
-    
+    return render_template('result.html', nombre=nombre, rating=rating, duracion=duracion)
 
 
-    print(nombre,"\\\\", rating, "\\", tiempo)
-
-    return render_template('result.html', nombre=nombre, rating=rating)
 
 
 
