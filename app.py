24015f69cf70e5ffdfdf437e38beddfd18ca1920
Tomas <xxxxx@xxxx.com>
2019-06-14 22:45:49 -0300

    fix url bug error

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -12,6 +12,7 @@ def index():
 
 @app.route('/<categoria>')
 def categoria(categoria):
+    url = 0
     if categoria == "horror":
         url = "https://www.imdb.com/search/title?genres=horror&sort=user_rating,desc&title_type=feature&num_votes=25000,"
     elif categoria == "action":
@@ -19,30 +20,33 @@ def categoria(categoria):
     elif categoria == "scifi":
         url = "https://www.imdb.com/search/title?genres=sci_fi&sort=user_rating,desc&title_type=feature&num_votes=25000,"
     
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, features="html.parser")
+    if url != 0:
 
-    x = random.randint(0,49)
-    
-    #nombre
-    llist = soup.find_all('h3',{'class':'lister-item-header'})
-    nombre = llist[x].find('a').text
-    #rating
-    llist = soup.find_all('div',{'class':'inline-block ratings-imdb-rating'})
-    rating = llist[x].find('strong').text
-    #duracion
-    llist = soup.find_all('span',{'class':'runtime'})
-    duracion = llist[x].text
-    #año
-    llist = soup.find_all('span',{'class':'lister-item-year'})   
-    anio = llist[x].text         
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.text, features="html.parser")
 
-    nombre += anio
+        x = random.randint(0,49)
+        
+        #nombre
+        llist = soup.find_all('h3',{'class':'lister-item-header'})
+        nombre = llist[x].find('a').text
+        #rating
+        llist = soup.find_all('div',{'class':'inline-block ratings-imdb-rating'})
+        rating = llist[x].find('strong').text
+        #duracion
+        llist = soup.find_all('span',{'class':'runtime'})
+        duracion = llist[x].text
+        #año
+        llist = soup.find_all('span',{'class':'lister-item-year'})   
+        anio = llist[x].text         
 
-    print(nombre,"\\\\", rating)
+        nombre += anio
 
-    return render_template('result.html', nombre=nombre, rating=rating, duracion=duracion)
+        print(nombre,"\\\\", rating)
 
+        return render_template('result.html', nombre=nombre, rating=rating, duracion=duracion)
+    else:   
+        abort(404)
 
 
 
